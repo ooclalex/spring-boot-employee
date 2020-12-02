1eaa506bf55479f618a2e7151bb5382f57e41e80
VICTOR CHU (CLOUD-ISD-OOCLL/HKG) <victor.chu@oocl.com>
2020-12-03 00:27:04 +0800

    [SpringBoot-Employee][Victor] feat:  add add company with same id

--- src/test/java/com/thoughtworks/springbootemployee/CompanyTests.java
+++ src/test/java/com/thoughtworks/springbootemployee/CompanyTests.java
@@ -5,17 +5,20 @@ import com.thoughtworks.springbootemployee.exception.DuplicatedIdException;
 import com.thoughtworks.springbootemployee.model.Company;
 import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
 import com.thoughtworks.springbootemployee.service.CompanyService;
+import com.thoughtworks.springbootemployee.service.EmployeeService;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CompanyTests {
     @Test
-    void should_return_company_when_add_company_given_no_employees(){
+    void should_return_company_when_add_company_given_no_employees() throws DuplicatedIdException {
         //given
         CompanyRepository companyRepository = new CompanyRepository();
         CompanyService companyService = new CompanyService(companyRepository);
@@ -26,5 +29,21 @@ public class CompanyTests {
 
         //then
         assertEquals(company, actual);
+    }
+
+    @Test
+    void should_return_error_when_add_company_given_company_with_same_id() throws DuplicatedIdException {
+        //given
+        CompanyRepository companyRepository = new CompanyRepository();
+        CompanyService companyService = new CompanyService(companyRepository);
+        Company company = new Company(1, "My Company", 1000, new ArrayList<Employee>());
+        companyService.add(company);
+        //when
+        final DuplicatedIdException duplicatedIdException = assertThrows(DuplicatedIdException.class,
+                () -> companyService.add(company)
+        );
+        //then
+        assertEquals("Duplicated ID", duplicatedIdException.getMessage());
+
     }
 }
