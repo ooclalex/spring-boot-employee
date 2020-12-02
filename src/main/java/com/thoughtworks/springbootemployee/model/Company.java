63b70b7ee7459e59bfc48a4429a59db606079a38
VICTOR CHU (CLOUD-ISD-OOCLL/HKG) <victor.chu@oocl.com>
2020-12-03 00:26:19 +0800

    [SpringBoot-Employee][Victor] feat:  add company getter and setter

--- src/main/java/com/thoughtworks/springbootemployee/model/Company.java
+++ src/main/java/com/thoughtworks/springbootemployee/model/Company.java
@@ -1,8 +1,47 @@
 package com.thoughtworks.springbootemployee.model;
 
+import com.thoughtworks.springbootemployee.service.EmployeeService;
+
 import java.util.ArrayList;
 
 public class Company {
+    private int id;
+    private String name;
+    private int employeeNumber;
+    private EmployeeService employees;
+
     public Company(int id, String name, int employeeNumber, ArrayList<Employee> employees) {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public void setEmployeeNumber(int employeeNumber) {
+        this.employeeNumber = employeeNumber;
+    }
+
+    public EmployeeService getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(EmployeeService employees) {
+        this.employees = employees;
     }
 }
