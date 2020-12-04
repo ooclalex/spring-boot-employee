8e7a9def1e5cd50ba576cb0863b61f05be032de2
VICTOR CHU (CLOUD-ISD-OOCLL/HKG) <victor.chu@oocl.com>
2020-12-04 15:20:39 +0800

    [SpringBoot-Employee][Victor] feat: add employee request and response

--- src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/EmployeeController.java
@@ -1,5 +1,8 @@
 package com.thoughtworks.springbootemployee.controller;
 
+import com.thoughtworks.springbootemployee.dto.EmployeeRequest;
+import com.thoughtworks.springbootemployee.dto.EmployeeResponse;
+import com.thoughtworks.springbootemployee.mapper.EmployeeMapper;
 import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/employees")
@@ -14,39 +18,44 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
+    private final EmployeeMapper employeeMapper;
+    public EmployeeController(EmployeeMapper employeeMapper){
+        this.employeeMapper = employeeMapper;
+    }
     @GetMapping
-    public List<Employee> getAll() {
-        return employeeService.getAll();
+    public List<EmployeeResponse> getAll() {
+        return employeeService.getAll().stream().map(employeeMapper::toResponse).collect(Collectors.toList());
     }
 
     @GetMapping(params = {"page", "pageSize"})
-    public List<Employee> getAllByPaging(
+    public List<EmployeeResponse> getAllByPaging(
             @RequestParam(required = false) Integer page,
             @RequestParam(required = false) Integer pageSize
     ) {
-        return employeeService.getAllByPage(page, pageSize).getContent();
+        return employeeService.getAllByPage(page, pageSize).getContent().stream().map(employeeMapper::toResponse).collect(Collectors.toList());
     }
 
     @GetMapping(params = {"gender"})
-    public List<Employee> getAllByGender(@RequestParam(required = false) String gender) {
-        return employeeService.getAllByGender(gender);
+    public List<EmployeeResponse> getAllByGender(@RequestParam(required = false) String gender) {
+        return employeeService.getAllByGender(gender).stream().map(employeeMapper::toResponse).collect(Collectors.toList());
     }
 
     @GetMapping("/{employeeId}")
-    public Employee getSpecificEmployee(@PathVariable String employeeId) {
-        return employeeService.get(employeeId);
+    public EmployeeResponse getSpecificEmployee(@PathVariable String employeeId) {
+        return employeeMapper.toResponse(employeeService.get(employeeId));
     }
 
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Employee create(@RequestBody Employee employeeUpdate) {
-        return employeeService.add(employeeUpdate);
+    public EmployeeResponse create(@RequestBody EmployeeRequest employeeUpdate) {
+        Employee employee = employeeService.add(employeeMapper.toEntity(employeeUpdate));
+        return employeeMapper.toResponse(employee);
     }
 
     @PutMapping("/{employeeId}")
-    public Employee update(@PathVariable String employeeId, @RequestBody Employee employeeUpdate) {
-        return employeeService.update(employeeId, employeeUpdate);
+    public EmployeeResponse update(@PathVariable String employeeId, @RequestBody Employee employeeUpdate) {
+        return employeeMapper.toResponse(employeeService.update(employeeId, employeeUpdate));
     }
 
     @DeleteMapping("/{employeeId}")
