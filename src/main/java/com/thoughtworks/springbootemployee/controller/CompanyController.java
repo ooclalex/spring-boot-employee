a59acdd13818f562292a833446eda00a3e9354c3
VICTOR CHU (CLOUD-ISD-OOCLL/HKG) <victor.chu@oocl.com>
2020-12-04 14:04:16 +0800

    [SpringBoot][Victor] Refactor: Remove unused libraries

--- src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -1,7 +1,6 @@
 package com.thoughtworks.springbootemployee.controller;
 
 import com.thoughtworks.springbootemployee.model.Company;
-import com.thoughtworks.springbootemployee.model.Employee;
 import com.thoughtworks.springbootemployee.service.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
